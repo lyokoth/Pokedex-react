3ee84745463c1b32ef98359cc96273d909765e2e
Lynette Okoth <lynokoth79@gmail.com>
2024-02-18 00:38:01 -0500

    change grid colors to match type

--- src/Components/Pokedex/PokeList.js
+++ src/Components/Pokedex/PokeList.js
@@ -1,17 +1,22 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { Grid, Flex, GridItem, Heading, Text, Input, IconButton } from '@chakra-ui/react';
+import { Grid, Flex, GridItem, Heading, Text, Input, IconButton, Spinner } from '@chakra-ui/react';
 import { SearchIcon } from '@chakra-ui/icons';
 import { Colors } from '../../Components/Routing/api';
 import gridLogo from '../../assets/pokeball-white.png';
 import './PokeList.css';
+import pokeball from '../../assets/pokeball-black.jpg';
 import { useNavigate } from 'react-router-dom';
 import PokedexContext  from '../../functions/Context';
+import Generation from './GenerationFilter/Generation';
+// add sidebar for generation filter
 
 
 const PokeList = () => {
   const [pokemon, setPokemon] = useState([]);
   const [search, setSearch] = useState('');
+  const [ selectedType, setSelectedType ] = useState('');
   const [selectedPokemon, setSelectedPokemon] = useState(null);
+  const [selectedGeneration, setSelectedGeneration] = useState('');
   const navigate = useNavigate();
 
   const {loading,
@@ -39,11 +44,26 @@ const PokeList = () => {
   }, []);
  
 
+  const filteredTypes = pokemon.filter((pokemon) => {
+    if (selectedType === "") {
+      return true;
+    } else {
+      return pokemon.types.some((type) => type.type.name === selectedType);
+    }
+  }).filter((pokemon) => {
+    if (search === "") {
+      return true;
+    } else {
+      return pokemon.name.toLowerCase().includes(search.toLowerCase());
+    }
+  });
+
   
 
 
   const handleSearchChange = (e) => {
     setSearch(e.target.value);
+   
   };
 
   const handlePokemonClick = async (pokemon) => {
@@ -65,9 +85,11 @@ const PokeList = () => {
   
   return (
     <div>
+  
+     <Heading size="lg" textAlign="center" mt="20px">Pokédex</Heading>
       <Flex justify="center" mt="20px">
         <Input
-          placeholder="Search by Pokémon name..."
+          placeholder="Search by Pokémon name, type, or id..."
           value={search}
           onChange={handleSearchChange}
           width={{ base: '80%', md: '50%' }}
@@ -77,9 +99,17 @@ const PokeList = () => {
           icon={<SearchIcon />}
           onClick={handleSearchChange} // This should trigger the search action
         />
+        <br />
+
+       
+    
       </Flex>
+
       <div className="pokedex-grid" style={{ textTransform: 'capitalize' }}>
-        {pokemon.length > 0 && (
+       
+        <span className="pokeball" style={{ backgroundImage: `url(${pokeball})` }}></span>
+        
+        {pokemon.length  > 0 && (
           <Grid templateColumns="repeat(3, 1fr)" gap={6} className="grid">
             {pokemon
               .filter((pokemon) => {
@@ -91,21 +121,23 @@ const PokeList = () => {
               })
               .map((pokemon, index) => (
                 <GridItem key={index} 
-                backgroundColor={'purple.100'} 
+                backgroundColor={Colors[pokemon.types[0].type.name]}
                 borderRadius="10px" 
                 p="20px" 
+            
                 border="1px solid black" 
                 onClick={() => handlePokemonClick(pokemon)}
                 style={{cursor: 'pointer'}}
+            
                 >
                     <img src={gridLogo} alt="Pokemon Logo" />
                   <Flex direction="column" align="center">
                   
                     <img src={pokemon.sprites.front_default} alt={pokemon.name} className='pkdexsprites' />
-                    <Heading size="md" mt="10px">{pokemon.name}</Heading>
-                    <Text>#{pokemon.id}</Text>
+                    <Heading size="lg" mt="10px" >{pokemon.name}</Heading>
+                    <span size="lg">#{pokemon.id}</span>
                     {pokemon.types && pokemon.types.map((type, i) => (
-                      <Text key={i} backgroundColor={Colors[type.type.name]} borderRadius={5} p={1} mt={2} textAlign="center"
+                      <Text key={i} backgroundColor={Colors[type.type.name]} borderRadius={5} p={1} mt={2} border="2px solid black" textAlign="center" flexDirection={'row' }
                       >{type.type.name}</Text>
                     ))}
                   </Flex>
