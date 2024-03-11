ddeca51d71080c29f456690bb44ae01b730a1e8b
Lynette Okoth <lynokoth79@gmail.com>
2024-03-10 21:35:43 -0400

    move FeaturedPokemon to its own folder

---
+++ src/Components/Home/FeaturedPokemon/FeaturedPokemon.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect, useContext } from 'react';
+import FeaturedPokemonCard from './FeaturedPokemonCard'; // Import FeaturedPokemonCard
+import PokedexContext from '../../../functions/Context'; 
+
+const FeaturedPokemon = () => {
+    const { loading, setLoading } = useContext(PokedexContext);
+    const [featuredPokemon, setFeaturedPokemon] = useState(null);
+    
+
+    useEffect(() => {
+        const fetchRandomPokemon = async () => {
+            setLoading(true);
+            try {
+                const randomPokemonId = Math.floor(Math.random() * 1025) + 1;
+                const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemonId}`);
+                const data = await response.json();
+                const speciesResponse = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${randomPokemonId}`);
+                const speciesData = await speciesResponse.json();
+                const evolution  = await fetch(`https://pokeapi.co/api/v2/evolution-chain/${randomPokemonId}`);
+                const evolutionData = await evolution.json();
+
+
+                
+                setFeaturedPokemon({...data, ...speciesData, ...evolutionData});
+
+            } catch (error) {
+                console.error("Could not fetch random Pokemon: ", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+
+        fetchRandomPokemon();
+     
+    }, [setLoading]);
+
+
+    return (
+        <>
+            <FeaturedPokemonCard featuredPokemon={featuredPokemon} loading={loading} />
+        </>
+    );
+};
+
+export default FeaturedPokemon;
