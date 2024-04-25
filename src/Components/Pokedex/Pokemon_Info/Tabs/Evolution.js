97b7513453954771a53fa765866ba6987c7e6418
Lynette Okoth <lynokoth79@gmail.com>
2024-04-25 01:42:55 -0400

    fetch data for evolutions

--- src/Components/Pokedex/Pokemon_Info/Tabs/Evolution.js
+++ src/Components/Pokedex/Pokemon_Info/Tabs/Evolution.js
@@ -1,65 +1,111 @@
-import { useContext, useState, useEffect } from "react";
-import { Flex, Stack, Card, Text, Spinner } from "@chakra-ui/react";
-import axios from "axios";
-import { ArrowBackIosTwoTone } from "@mui/icons-material";
+import React, { useEffect, useState } from 'react';
+import { Colors } from '../../../Routing/api';
+import axios from 'axios';
+import {  Stack, Text, Image, Card, Flex} from "@chakra-ui/react";
+import { ArrowRightIcon } from '@chakra-ui/icons';
+import { useNavigate } from 'react-router-dom';
 
 const Evolution = ({ pokemon }) => {
+     const {evolution_chain, type} = pokemon;
+    
+     const [evolution, setEvolution] = useState([]);
+     const [preEvolution, setPreEvolution] = useState([]);
+     const [finalEvolution, setFinalEvolution] = useState([]);
+
+     const navigate = useNavigate();
+
+     // parse url to get the evolution id 
+
+        useEffect(() => {
+            const fetchEvolution = async () => {
+                try {
+                    const res = await axios.get(evolution_chain.url);
+                    const data = res.data;
+                    console.log(data);
+                    const evolution = data.chain.evolves_to.map(evolution => evolution.species.name);
+                    const preEvolution = data.chain.evolves_from ? data.chain.evolves_from.species.name : evolution[0];
+                    const finalEvolution = data.chain.evolves_to[0].evolves_to[0] ? data.chain.evolves_to[0].evolves_to[0].species.name : null;
+                    setEvolution(evolution);
+                    setFinalEvolution(finalEvolution);
+              
+    
+
+                    console.log(evolution);
+                    console.log(preEvolution);
+                    console.log(finalEvolution);
+                } catch (error) {
+                    console.log(error);
+                }
+            };
+
+            fetchEvolution();
+        }, [evolution_chain]);
+
+        if (!evolution) return <div>This Pokemon does not evolve.</div>
+
+        if (evolution.length === 0) return <div>This Pokemon does not evolve.</div>
+
+    
+        
+     
+        const handleEvolutionClick = (name) => {
+            navigate(`/pokemon/${id}`);
+        }
+        // defining the attributes for the evolved pokemon
+        const {sprites, name, front_default, id, data} = evolution;
+      
+       
  
-    useEffect(() => {
-        const fetchEvolution = async () => {
-            try {
-                const evol_response = await axios.get(`https://pokeapi/v2/pokemon-species/${pokemon.id}`);
-                const evol_data = evol_response.data;
-                const evol_chain_url = evol_data.evolution_chain.url;
-                console.log(evol_chain_url);
-        
-                const evol_chain_response = await axios.get(evol_chain_url);
-           
-                const evol_chain_data = evol_chain_response.data;
-        
-                const chain = evol_chain_data.chain;
-        
-                const processChain = (chain) => {
-                    console.log("Species:", chain.species);
-                    chain.evolves_to.forEach(evolves_to => {
-                        console.log("Evolution Details:", evolves_to.evolution_details);
-                        processChain(evolves_to);
-                    });
-                };
-        
-                processChain(chain);
-        
-            } catch (error) {
-                console.log(error);
-            }
-        } 
-        fetchEvolution();
-    }
-    , [pokemon]);
+        return (
+               <Card style={{border: '1px'}}>
+                    <Stack direction="row" spacing={2}>
+                        <Flex flexDirection="row" alignItems="center">
+                        {evolution.map((evolution, index) => (
+                        <div key={index}>
+                             <img
+                             
+                             src={pokemon.sprites.front_default} 
+                            
+                             
+                             alt={pokemon.name} />  
+                            <Text 
+                                className="evo-name"
+                                key={index} 
+                                textTransform={'capitalize'}
+                                style={{ backgroundColor: Colors[evolution] }}>{pokemon.name}</Text>   
+                                
+                                 <ArrowRightIcon />
+                       </div>
+                      
+                    
+                        ))}
+                      <div>
+                     <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+                        <Text 
+                            className="evo-name"
+                            textTransform={'capitalize'}
+                            style={{ backgroundColor: Colors[pokemon.name] }}>{evolution}</Text>
 
+                        </div>
+                        < ArrowRightIcon />
+                        </Flex>
 
-return (
-    <>
-   
-    <Text>{pokemon.evolution_chain.url}</Text>
-    <Flex direction={'column'}>
-        <Stack direction={{base: "column", md: "column"}}>
-            <Card className="stats" style={{textTransform: 'capitalize'}}>
-                <Text>Evolution Chain:</Text>
-                {pokemon.evolution_chain.url ? (
-                    <Spinner color='purple.500' speed="0.65s" emptyColor='gray.200' size='xl' />
-                ) : (
-                    <Text>Evolution Chain not available</Text>
-                ) }
-            </Card>
-        </Stack>
-    </Flex>
-    
-    
-</>
-);
+                       
+                 <div>
+                    <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+                    <Text 
+                    className="evo-name"
+                    textTransform={'capitalize'}
+                    style={{ backgroundColor: Colors[finalEvolution] }}>{finalEvolution}</Text>
+                    </div>
+                    </Stack>
+                </Card>
+                
+        
+                   
+         
+        );
 }
 export default Evolution;
-
-
-
+    
+           
